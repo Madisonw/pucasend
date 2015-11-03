de54e1008a9feddec95f5629a514203470a24dc9
Madison Williams <madisonjw@gmail.com>
2015-11-03 12:12:39 -0800

    updating to the Elliot styles

--- options.js
+++ options.js
@@ -1,19 +1,11 @@
 
 function save_options() {
-
   var valueThreshold = document.getElementById('valueThreshold').value;
   var likesColor = document.getElementById('onoff').checked;
   
   chrome.storage.sync.set({
     valueThreshold: valueThreshold,
     onoff: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
   });
 }
 
@@ -29,5 +21,6 @@ function restore_options() {
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+
+document.getElementById('valueThreshold').addEventListener("keyup", save_options);
+document.getElementById('onoff').addEventListener("change", save_options);
